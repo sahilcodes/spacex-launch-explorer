dfedc59fd9f1d2a9c8aa2fe90e310044a703d78f
sahilcodes <sahilcancode@gmail.com>
2025-03-27 00:19:29 +0530

    Added Home.tsx file and did changes in App.tsx file

---
+++ src/pages/Home.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { useLaunches } from "../hooks/useLaunches";
+import LaunchCard from "../components/LaunchCard";
+
+const Home: React.FC = () => {
+  const { launches, loading } = useLaunches();
+  const [query, setQuery] = useState("");
+
+  if (loading) return <p>Loading launches...</p>;
+
+  return (
+    <>
+      <input
+        type="text"
+        placeholder="Search by mission name..."
+        onChange={(e) => setQuery(e.target.value.toLowerCase())}
+      />
+
+      <div className="launch-list">
+        {launches
+          .filter((launch) => launch.name.toLowerCase().includes(query))
+          .map((launch) => (
+            <LaunchCard key={launch.id} launch={launch} />
+          ))}
+      </div>
+    </>
+  );
+};
+
+export default Home;
